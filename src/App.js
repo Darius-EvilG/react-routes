c04b6123cc3d34a91989a208cb515d4c27574325
RICARDO <ricardogarciaherbas@gmail.com>
2024-10-19 10:31:03 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Inicio from './paginas/Inicio';
+import Acerca from './paginas/Acerca';
+import Contacto from './paginas/Contacto';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Inicio</Link></li>
+          <li><Link to="/acerca">Acerca de</Link></li>
+          <li><Link to="/contacto">Contacto</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/acerca" element={<Acerca />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/paginas/Acerca.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Acerca() {
+  return <h1>Página de Acerca de</h1>;
+}
+
+export default Acerca;
